631c033a5f0a0a0895323ed07d4a27d0184400db
Kutala Franse <kfranse@kutalas-mbp.lan>
2022-01-19 19:33:07 -0500

    Completed Flask routes for temperature data

--- app.py
+++ app.py
@@ -77,7 +77,7 @@ def temp_monthly():
     return jsonify(temps=temps)
 
 # Statistics Report
-@app.route("/api/v1.0/temp/<start>")
+#@app.route("/api/v1.0/temp/<start>")
 @app.route("/api/v1.0/temp/<start>/<end>")
 def stats(start=None, end=None):
     sel = [func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)]
